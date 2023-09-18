48de8a3e026baa76e9da8a146189036719125380
LuisaCelis <luisafernandacelisgonzalez@gmail.com>
2023-09-17 22:04:06 -0500

    Modulo pacientes funcional

--- src/app/modelos/paciente.ts
+++ src/app/modelos/paciente.ts
@@ -2,7 +2,7 @@ import { Eps } from "./eps";
 import { Persona } from "./persona";
 
 export class Paciente{
-    id_paciente?: number;
-    codigo_persona?: Persona = new Persona();
+    idPaciente: number;
+    persona: Persona = new Persona();
     eps?: Eps = new Eps();
 }
