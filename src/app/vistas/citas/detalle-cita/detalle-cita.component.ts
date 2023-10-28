f1be2435c48e5b1bd1eb46161d19e7c013516b6b
DidierLLantenPc <didiervelez@hotmail.es>
2023-10-28 16:16:31 -0500

    Titulo: HU:08 - Consultar detalle cita paciente. Descripcion: Componente actualizado para mostrar n cantidad de citas por confirmar

--- src/app/vistas/citas/detalle-cita/detalle-cita.component.ts
+++ src/app/vistas/citas/detalle-cita/detalle-cita.component.ts
@@ -15,74 +15,25 @@ export class DetalleCitaComponent {
     private messageService: MessageService
   ) {}
 
-  citaEncontrada: Cita;
+  citasEncontradas: Cita[] | undefined;
   cedula: string;
 
   buscarCitaPorCedula() {
-    console.log('Buscada');
     this.citaService
       .obtenerCitasPorCedulaYIdEstadoCita(this.cedula, 3)
       .subscribe((dato) => {
-        console.log('Citas: ', dato);
-        this.citaEncontrada = dato[0];
         if (dato.length == 0) {
+          this.citasEncontradas = undefined;
           this.messageService.add({
             severity: 'info',
             summary: 'No encontrada',
             detail: 'El paciente no tiene citas asignadas',
             life: 3000,
           });
+        } else {
+          this.citasEncontradas = dato;
         }
       });
-
-    // this.citaEncontrada = {
-    //   codigo: 4,
-    //   paciente: {
-    //     idPaciente: 8,
-    //     persona: {
-    //       codigo: 11,
-    //       nombre: 'Kelwin',
-    //       apellido: 'Durrington',
-    //       tipo_identificacion: {
-    //         codigo: 2,
-    //         tipo: 'Tarjeta de identidad',
-    //       },
-    //       numero_documento: '6498439841',
-    //       telefono: '3519980861',
-    //       correo: 'kdurrington1@nymag.com',
-    //     },
-    //     eps: {
-    //       id: 3,
-    //       nombre: 'Coomeva',
-    //       nit: '56465466',
-    //     },
-    //   },
-    //   medico: {
-    //     idMedico: 14,
-    //     persona: {
-    //       codigo: 109,
-    //       nombre: 'Kylie',
-    //       apellido: 'Fricke',
-    //       tipo_identificacion: {
-    //         codigo: 1,
-    //         tipo: 'Cedula de ciudadania',
-    //       },
-    //       numero_documento: '1153708701',
-    //       telefono: '9314201571',
-    //       correo: 'kfricke2r@ask.com',
-    //     },
-    //     especialidadMedico: {
-    //       id: 2,
-    //       nombre: 'NUTRICIONISTA',
-    //     },
-    //     tarjetaProfesional: '77640',
-    //   },
-    //   fecha: new Date('2023-09-28T19:30:00'),
-    //   estadoCita: {
-    //     id: 3,
-    //     nombre: 'ASIGNADA',
-    //   },
-    // };
   }
 
   getColorTagEstado(estado: string | undefined): string {
